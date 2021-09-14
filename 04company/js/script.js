3b7c8b237d72457baa7a7cda699889973d4b765b
yon9woo <sevll5953@naver.com>
2021-09-14 13:37:17 +0900

    d

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-    $(".slider").bxSlider();
+    const slider1 = $(".slider").bxSlider({
+        controls:false
+    });
+    $(".nextBtn").click(function(){
+        slider1.goToNextSlide();
+    });$(".prevBtn").click(function(){
+        slider1.goToPrevSlide();
+    });
+    $(".toggle").click(function(){
+        const toggle = $(".toggle").attr("class"); // value toggle
+        console.log(toggle);
+        if( toggle == "toggle"){
+            // $(".toggle").addClass("active");
+            document.getElementsByClassName("toggle")[0].classList.add("active");
+            $(".gnb").addClass("view");
+        }else{
+            // $(".toggle").removeClass("active");
+            document.getElementsByClassName("toggle")[0].classList.remove("active");
+            $(".gnb").removeClass("view");
+        }
+    });
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
+    });
 });
