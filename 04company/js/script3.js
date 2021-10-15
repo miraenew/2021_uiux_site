ce55001f6f1f9aab0514c1fc435223cf23d881ba
yon9woo <sevll5953@naver.com>
2021-10-15 18:29:51 +0900

    f

--- 04company/js/script3.js
+++ 04company/js/script3.js
@@ -7,10 +7,33 @@ $(document).ready(function(){
         controls:false,
         pager:false
     });
+
+    if(window.innerWidth > 700){
     $(".mainLi").mouseover(function(){
         $(".subMenu").stop().slideDown();
     });
     $(".mainLi").mouseout(function(){
         $(".subMenu").stop().slideUp();
-    })
+    });
+}
+
+
+    /////////////////////////////////////
+    $(".toggle").click(function(){
+        var toggle = $(".toggle").attr("class");
+
+    if( toggle == "toggle"){
+        $(".toggle").addClass("active");
+        $(".gnb").show();
+        $(".gnb").animate({"left":"0"},300);
+    }else {
+        $(".toggle").removeClass("active");
+        $(".gnb").hide();
+        $(".gnb").animate({"left":"-100%"},300);
+    }
+    });
+    $(window).resize(function(){
+        $(".gnb").attr("style","");
+        $(".toggle").removeClass("active");
+    });
 });
